1d4e0861674e3ae0f7793a54afef9b1fdb3a6e81
Hemashree-G <hemashreeg019@gmail.com>
2025-07-19 16:26:35 +0530

    Add files via upload

---
+++ Problem_2.java
@@ -0,0 +1,24 @@
+package in.HemashreeG;
+
+import java.util.Scanner;
+
+public class Problem_2 {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("eneter the no");
+		int a=sc.nextInt();
+		int num=1;
+		for(int i=0;i<a;i++) {
+			System.out.println(num);
+			num=num+2;
+		}
+
+	}
+	}
+
+		
+
+	
+
+
+
